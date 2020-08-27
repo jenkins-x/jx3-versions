bca9405f5dd913a6514fe36580f2a659f7c9b58a
James Strachan <james.strachan@gmail.com>
2020-08-27 18:22:18 +0100

    fix: for kind

--- jx/bdd/kind/ci.sh
+++ jx/bdd/kind/ci.sh
@@ -49,7 +49,7 @@ jx upgrade
 export BDD_NAME="bdd-kind"
 
 # lets default env vars that don't get populated if not running in jx
-export BRANCH_NAME="${BRANCH_NAME:-pr}"
+export BRANCH_NAME="${BRANCH_NAME:-pr-todo}"
 export BUILD_NUMBER="${BUILD_NUMBER:-1}"
 
 # the gitops repository template to use
