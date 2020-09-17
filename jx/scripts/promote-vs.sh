cbfcf675af736a8f30da63bb92e4a2d55c7d3b7d
James Strachan <james.strachan@gmail.com>
2020-09-17 11:52:51 +0100

    fix: promote script

--- jx/scripts/promote-vs.sh
+++ jx/scripts/promote-vs.sh
@@ -6,12 +6,12 @@ set -e
 echo "promoting changes in jx3-gitops-template to downstream templates"
 
 declare -a repos=(
+  # local
+  "jx3-kubernetes" "jx3-kind-vault" "jx3-minikube-vault" "jx3-docker-vault"
   # GKE
   "jx3-gke-terraform-vault" "jx3-gke-gcloud-vault" 
   # EKS
   "jx3-eks-terraform-vault"
-  # local
-  "jx3-kind-vault" "jx3-minikube-vault" "jx3-docker-vault"
 )
 
 export TMPDIR=/tmp/jx3-gitops-promote
@@ -25,7 +25,14 @@ do
   cd $TMPDIR
   git clone https://github.com/jx3-gitops-repositories/$r.git
   cd "$r"
+
+  echo "recreating a clean version stream"
+  rm -rf versionStream
   jx gitops kpt update || true
+
+  kpt pkg get https://github.com/jenkins-x/jxr-versions.git/ versionStream
+  rm -rf versionStream/jenkins*.yml versionStream/jx versionStream/.github versionStream/.pre* versionStream/.secrets* versionStream/OWNER*
+  
   git add * || true
   git commit -a -m "chore: upgrade version stream" || true
   git push || true
