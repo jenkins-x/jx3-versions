df26fb8cb53a50ae5c70c9c1581c7341332813e7
James Rawlings <rawlingsj80@gmail.com>
2020-09-21 11:28:50 +0100

    feat: lets enable multi repo infra + cluster terraform BDD tests

--- jx/bdd/gke/ci.sh
+++ jx/bdd/gke/ci.sh
@@ -3,13 +3,13 @@ set -e
 set -x
 
 # BDD test specific part
-export BDD_NAME="bdd-gke"
+# export BDD_NAME="bdd-gke"
 
-# the gitops repository template to use
-#export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-gke-gcloud-vault"
-export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-gke-terraform-vault"
+# # the gitops repository template to use
+# #export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-gke-gcloud-vault"
+# export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-gke-terraform-vault"
 
-# to enable spring / gradle...
-#export RUN_TEST="bddjx -ginkgo.focus=spring-boot-http-gradle -test.v"
+# # to enable spring / gradle...
+# #export RUN_TEST="bddjx -ginkgo.focus=spring-boot-http-gradle -test.v"
 
-`dirname "$0"`/../ci.sh
+# `dirname "$0"`/../ci.sh
---
+++ jx/bdd/terraform-ci.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+echo PATH=$PATH
+echo HOME=$HOME
+
+export PATH=$PATH:/usr/local/bin
+
+# generic stuff...
+
+# setup environment
+KUBECONFIG="/tmp/jxhome/config"
+
+#export XDG_CONFIG_HOME="/builder/home/.config"
+mkdir -p /builder/home
+mkdir -p /home/.config
+cp -r /home/.config /builder/home/.config
+
+jx version
+jx help
+
+export JX3_HOME=/home/.jx3
+jx admin --help
+jx secret --help
+
+
+
+
+if [ -z "$GIT_USERNAME" ]
+then
+    export GIT_USERNAME="jenkins-x-labs-bot"
+fi
+
+if [ -z "$GIT_SERVER_HOST" ]
+then
+    export GIT_SERVER_HOST="github.com"
+fi
+
+if [ -z "$GH_OWNER" ]
+then
+    export GH_OWNER="cb-kubecd"
+fi
+
+export GIT_USER_EMAIL="jenkins-x@googlegroups.com"
+export GIT_TOKEN="${GH_ACCESS_TOKEN//[[:space:]]}"
+export GIT_PROVIDER_URL="https://${GIT_SERVER_HOST}"
+
+
+if [ -z "$GIT_TOKEN" ]
+then
+      echo "ERROR: no GIT_TOKEN env var defined for bdd/terraform-ci.sh"
+else
+      echo "has valid git token in bdd/terraform-ci.sh"
+fi
+
+export PROJECT_ID=jenkins-x-labs-bdd
+export CREATED_TIME=$(date '+%a-%b-%d-%Y-%H-%M-%S')
+export CLUSTER_NAME="${BRANCH_NAME,,}-$BUILD_NUMBER-$BDD_NAME"
+export ZONE=europe-west1-c
+export LABELS="branch=${BRANCH_NAME,,},cluster=$BDD_NAME,create-time=${CREATED_TIME,,}"
+
+# lets setup git
+git config --global --add user.name jenkins-x-bot
+git config --global --add user.email jenkins-x@googlegroups.com
+
+echo "running the BDD test with JX_HOME = $JX_HOME"
+
+mkdir -p $XDG_CONFIG_HOME/git
+# replace the credentials file with a single user entry
+echo "https://${GIT_USERNAME//[[:space:]]}:${GIT_TOKEN}@${GIT_SERVER_HOST}" > $XDG_CONFIG_HOME/git/credentials
+
+echo "using git credentials: $XDG_CONFIG_HOME/git/credentials"
+ls -al $XDG_CONFIG_HOME/git/credentials
+
+echo "creating cluster $CLUSTER_NAME in project $PROJECT_ID with labels $LABELS"
+
+echo "lets get the PR head clone URL"
+export PR_SOURCE_URL=$(jx gitops pr get --git-token=$GIT_TOKEN --head-url)
+
+echo "using the version stream url $PR_SOURCE_URL ref: $PULL_PULL_SHA"
+
+export GITOPS_TEMPLATE_URL="https://github.com/${GITOPS_TEMPLATE_PROJECT}.git"
+
+# create the boot git repository to mimic creating the git repository via the github create repository wizard
+jx admin create -b --initial-git-url $GITOPS_TEMPLATE_URL --env dev --version-stream-ref=$PULL_PULL_SHA --version-stream-url=${PR_SOURCE_URL//[[:space:]]} --env-git-owner=$GH_OWNER --repo env-$CLUSTER_NAME-dev --no-operator $JX_ADMIN_CREATE_ARGS
+
+export GITOPS_REPO=https://${GIT_SERVER_HOST}/${GH_OWNER}/env-${CLUSTER_NAME}-dev.git
+
+echo "going to clone git repo $GITOPS_REPO"
+
+if [ -z "$NO_JX_TEST" ]
+then
+    jx test create --test-url $GITOPS_REPO
+
+    # lets garbage collect any old tests or previous failed tests of this repo/PR/context...
+    jx test gc
+else
+      echo "not using jx-test to gc old tests"
+fi
+
+export SOURCE_DIR=`pwd`
+
+# avoid cloning cluster repo into the working CI folder
+cd ..
+
+git clone $GITOPS_REPO
+cd env-${CLUSTER_NAME}-dev
+
+# use the changes from this PR in the version stream for the cluster repo when resolving the helmfile
+rm -rf versionStream
+cp -R $SOURCE_DIR versionStream
+rm -rf versionStream/.git versionStream/.github
+git add versionStream/
+
+
+# lets add some testing charts....
+jx gitops helmfile add --chart jx3/jx-test-collector
+
+# lets add / commit any cloud resource specific changes
+git add * || true
+git commit -a -m "chore: cluster changes" || true
+git push
+
+cd ..
+
+export GITOPS_INFRA_URL="https://github.com/${GITOPS_INFRA_PROJECT}.git"
+
+echo "going to clone git repo $GITOPS_INFRA_URL"
+
+git clone $GITOPS_INFRA_URL ${CLUSTER_NAME}-infra
+cd ${CLUSTER_NAME}-infra
+# lets protect against pushing local files like state etc to a git repo
+rm -rf .git
+
+# lets configure the terraform module
+export TF_VAR_gcp_project=$PROJECT_ID
+export TF_VAR_cluster_name=$CLUSTER_NAME
+export TF_VAR_jx_git_url=$GITOPS_REPO
+export TF_VAR_jx_bot_username=$GIT_USERNAME
+export TF_VAR_jx_bot_token=$GIT_TOKEN
+
+
+export GITOPS_DIR=`pwd`
+export GITOPS_BIN=$GITOPS_DIR/bin
+
+
+# lets create the cluster
+$GITOPS_BIN/create.sh
+
+$(terraform output connect)
+
+$(terraform output follow_install_logs)
+
+jx ns jx
+
+# lets wait for things to be installed correctly
+make verify-install
+
+jx secret verify
+
+# diagnostic commands to test the image's kubectl
+kubectl version
+
+# for some reason we need to use the full name once for the second command to work!
+kubectl get environments
+kubectl get env dev -oyaml
+kubectl get cm config -oyaml
+
+export JX_DISABLE_DELETE_APP="true"
+export JX_DISABLE_DELETE_REPO="true"
+
+# increase the timeout for complete PipelineActivity
+export BDD_TIMEOUT_PIPELINE_ACTIVITY_COMPLETE="60"
+
+# we don't yet update the PipelineActivity.spec.pullTitle on previews....
+export BDD_DISABLE_PIPELINEACTIVITY_CHECK="true"
+
+# define variables for the BDD tests
+export GIT_ORGANISATION="$GH_OWNER"
+export GH_USERNAME="$GIT_USERNAME"
+
+# lets turn off color output
+export TERM=dumb
+
+echo "about to run the bdd tests...."
+
+
+# run the BDD tests
+if [ -z "$RUN_TEST" ]
+then
+      #bddjx -ginkgo.focus=golang -test.v
+else
+      $RUN_TEST
+fi
+
+echo "completed the bdd tests"
+
+echo "switching context back to the infra cluster"
+
+# lets connect back to the infra cluster so we can find the TestRun CRDs
+#gcloud container clusters get-credentials flash --zone europe-west1-b --project jx-labs-infra
+#gcloud container clusters get-credentials tf-jx-growing-ant --zone us-central1-a --project jx-labs-infra
+gcloud container clusters get-credentials tf-jx-gentle-titmouse --zone us-central1-a --project jx-labs-infra
+
+jx ns jx
+
+if [ -z "$NO_JX_TEST" ]
+then
+    echo "cleaning up cloud resources"
+    jx test delete --test-url $GITOPS_REPO --dir=$GITOPS_DIR --script=$GITOPS_BIN/destroy.sh
+else
+    echo "not using jx-test to gc test resources"
+fi
