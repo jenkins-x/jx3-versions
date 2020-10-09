ea901e5deff4532002306e51d5fd9e2578ef4224
James Strachan <james.strachan@gmail.com>
2020-10-09 15:16:26 +0100

    fix: add the new step to publish requirements to dev env

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -61,6 +61,9 @@ fetch: init
 	# lets make sure all the namespaces exist for environments of the replicated secrets
 	jx gitops namespace --dir-mode --dir $(OUTPUT_DIR)/namespaces
 
+	# lets publish the requirements metadata into the dev Environment.Spec.TeamSettings.BootRequirements so its easy to access them via CRDs
+	jx gitops requirements publish
+
 .PHONY: build
 # uncomment this line to enable kustomize
 #build: build-kustomise
