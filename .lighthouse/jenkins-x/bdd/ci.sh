4afc0d6c0598faa8ad1a8b9101d8f353931ee164
James Strachan <james.strachan@gmail.com>
2020-10-20 09:46:11 +0100

    fix: try fix kube script

--- .lighthouse/jenkins-x/bdd/ci.sh
+++ .lighthouse/jenkins-x/bdd/ci.sh
@@ -121,7 +121,7 @@ export SOURCE_DIR=`pwd`
 # avoid cloning cluster repo into the working CI folder
 cd /workspace
 
-git clone $GITOPS_REPO
+git clone -b master $GITOPS_REPO env-${CLUSTER_NAME}-dev
 cd env-${CLUSTER_NAME}-dev
 
 # use the changes from this PR in the version stream for the cluster repo when resolving the helmfile
@@ -133,7 +133,7 @@ git add versionStream/
 
 # lets add some testing charts....
 echo "about to add helm chart in dir $(pwd)"
-echo ls -al *.yml
+ls -al
 
 jx gitops helmfile add --chart jx3/jx-test-collector
 
