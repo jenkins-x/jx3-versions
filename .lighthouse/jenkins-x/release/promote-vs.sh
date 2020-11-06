3dbfaff2fbd1660e54620d8b12cad5f1d3f825a8
James Rawlings <rawlingsj80@gmail.com>
2020-11-06 11:44:52 +0000

    chore: try and fix auto upgrade of eagle

--- .lighthouse/jenkins-x/release/promote-vs.sh
+++ .lighthouse/jenkins-x/release/promote-vs.sh
@@ -45,6 +45,6 @@ cd $TMPDIR
 git clone https://github.com/jenkins-x/jx3-eagle.git
 cd "jx3-eagle"
 git checkout -b $LOCAL_BRANCH_NAME
-jx gitops upgrade
+jx gitops upgrade --commit-message "chore: version stream upgrade"
 git push origin $LOCAL_BRANCH_NAME
 jx create pullrequest -t "chore: version stream upgrade" -l updatebot
