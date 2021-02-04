ac3ffb2579791ba8cffa3522267b0b6dde729c80
Chris Mellard <chrism@datacom.co.nz>
2021-02-04 23:31:53 +1300

    feat: bring secret population earlier in boot sequence
    use external-secrets fork that is compatible with cross namespace vault access

--- .lighthouse/jenkins-x/bdd/vault/ci.sh
+++ .lighthouse/jenkins-x/bdd/vault/ci.sh
@@ -6,7 +6,7 @@ set -x
 export BDD_NAME="gke-vault"
 
 # the gitops repository template to use
-export GITOPS_INFRA_PROJECT="jx3-gitops-repositories/jx3-terraform-gke"
-export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-gke-vault"
+export GITOPS_INFRA_PROJECT="chrismellard/jx3-terraform-gke"
+export GITOPS_TEMPLATE_PROJECT="chrismellard/jx3-gke-vault"
 
 `dirname "$0"`/../terraform-ci.sh
