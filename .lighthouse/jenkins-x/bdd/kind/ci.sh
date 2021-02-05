9cc0350b8d2248674bc1f48094500d889f08554b
James Strachan <james.strachan@gmail.com>
2021-02-05 12:03:34 +0000

    chore: update the go upgrade

--- .lighthouse/jenkins-x/bdd/kind/ci.sh
+++ .lighthouse/jenkins-x/bdd/kind/ci.sh
@@ -67,6 +67,6 @@ export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-kind-vault"
 
 mkdir -p /builder/home
 
-export RUN_TEST=".lighthouse/jenkins-x/bdd/test.sh"
+export RUN_TEST=".lighthouse/jenkins-x/bdd/kind/test.sh"
 
 .lighthouse/jenkins-x/bdd/ci.sh
