bbc7cde70d7f1227c07798d1f029d78d89a66557
James Strachan <james.strachan@gmail.com>
2021-02-16 16:21:39 +0000

    chore: fix EKS

--- .lighthouse/jenkins-x/bdd/eks/ci.sh
+++ .lighthouse/jenkins-x/bdd/eks/ci.sh
@@ -9,6 +9,7 @@ export BDD_NAME="eks-vault"
 export GITOPS_INFRA_PROJECT="jx3-gitops-repositories/jx3-terraform-eks"
 export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-eks-vault"
 
+export TF_VAR_region="us-east-1"
 export TERRAFORM_FILE="terraform-eks.yaml.gotmpl"
 
 `dirname "$0"`/../terraform-ci.sh
