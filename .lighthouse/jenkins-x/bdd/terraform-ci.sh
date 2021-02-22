beec25215919badf1688fd0e38246f3fef6aa4cc
James Strachan <james.strachan@gmail.com>
2021-02-22 16:28:11 +0000

    fix: add kapp BDD test

--- .lighthouse/jenkins-x/bdd/terraform-ci.sh
+++ .lighthouse/jenkins-x/bdd/terraform-ci.sh
@@ -143,6 +143,11 @@ else
       #gh auth login --hostname $GIT_SERVER_HOST --with-token $GH_ACCESS_TOKEN
 fi
 
+if [ -z "$JX_GIT_OVERRIDES" ]
+then
+    export JX_GIT_OVERRIDES="echo no git overrides"
+fi
+
 
 $JX_SCM repo create ${GH_HOST}${GH_OWNER}/cluster-$CLUSTER_NAME-dev --template $GIT_TEMPLATE_SERVER_URL/${GITOPS_TEMPLATE_PROJECT} --private --confirm
 sleep 15
@@ -165,6 +170,10 @@ pushd `pwd`/cluster-${CLUSTER_NAME}-dev
 
       # lets upgrade any versions in helmfile.yaml
       jx gitops helmfile resolve --update
+
+
+      # any git repo overrides...
+      $JX_GIT_OVERRIDES
 
       # lets add a custom pipeline catalog for the test...
       #cp $SOURCE_DIR/.lighthouse/jenkins-x/bdd/pipeline-catalog.yaml extensions
