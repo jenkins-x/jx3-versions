632cb62c89933fe58fc977d1944bb13bbd94e681
James Strachan <james.strachan@gmail.com>
2021-04-12 16:20:09 +0100

    chore: fix file name

--- .lighthouse/jenkins-x/bdd/kubevault/ci.sh
+++ .lighthouse/jenkins-x/bdd/kubevault/ci.sh
@@ -8,7 +8,7 @@ export BDD_NAME="bdd-kubevault"
 # the gitops repository template to use
 export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-kubernetes-vault"
 
-export TERRAFORM_FILE="terraform-kube-vault.yaml.gotmpl"
+export TERRAFORM_FILE="terraform-kubevault.yaml.gotmpl"
 
 export PROJECT_ID=jenkins-x-labs-bdd1
 export TF_VAR_project_id=$PROJECT_ID
