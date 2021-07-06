5ab13af6d02bc371a26c16d931acd83731748679
James Strachan <james.strachan@gmail.com>
2021-07-06 15:44:42 +0100

    fix:

--- .lighthouse/jenkins-x/release/promote-jx-website.sh
+++ .lighthouse/jenkins-x/release/promote-jx-website.sh
@@ -9,3 +9,4 @@ git clone https://github.com/jenkins-x/jx-docs.git
 cd jx-docs
 sed -i "s/release = \".*\"/release = \"${JX_VERSION}\"/" config.toml
 git commit --allow-empty -a -m "chore: upgrade jx version"
+git push
