b226d969de5b4e7e1b71b974e1da6c0e220aa35f
James Strachan <james.strachan@gmail.com>
2021-07-06 15:02:38 +0100

    fix: try upgrade the version on the website on promote

---
+++ .lighthouse/jenkins-x/release/promote-jx-website.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+pushd $(mktemp -d)
+  git clone https://github.com/jenkins-x/jx-docs.git
+
+  pushd jx-docs
+    sed -i "s/release = \".*\"/release = \"${JX_VERSION}\"/" config.toml
+    git commit --allow-empty -a -m "chore: upgrade jx version"
+    git push
+  popd
+popd
