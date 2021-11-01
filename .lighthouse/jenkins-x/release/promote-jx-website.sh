c6a08ffcc704fcad16a0960e184f10850f7a111b
Anatoli Babenia <anatoli@rainforce.org>
2021-11-01 18:22:50 +0300

    fix: do not push empty commits

--- .lighthouse/jenkins-x/release/promote-jx-website.sh
+++ .lighthouse/jenkins-x/release/promote-jx-website.sh
@@ -8,5 +8,5 @@ git clone https://github.com/jenkins-x/jx-docs.git
 
 cd jx-docs
 sed -i "s/release = \".*\"/release = \"${JX_VERSION}\"/" config.toml
-git commit --allow-empty -a -m "chore: upgrade jx version"
+git commit -a -m "chore: upgrade jx version"
 git push
