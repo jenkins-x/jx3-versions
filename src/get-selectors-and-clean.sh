1e26d932f245e758b822d49f9ec1f3d69f4c24c6
Skisocks <jaa.skeoch@gmail.com>
2023-03-09 14:02:54 +0000

    fix: make indentation consistent

--- src/get-selectors-and-clean.sh
+++ src/get-selectors-and-clean.sh
@@ -5,18 +5,18 @@ OUTPUT_DIR=$1
 SELECTOR=""
 if ! git log -m -1 --name-only --pretty=format: | grep -qv "^helmfiles/"; then
   changedNamespaces=$(git log -m -1 --name-only --pretty=format: | grep "^helmfiles/" | cut -d "/" -f 2 | sort -u)
-    if echo "$changedNamespaces" | grep -q ^jx$; then
-        >&2 echo jx namespace changed, no selectors added
-    else
-      for namespace in ${changedNamespaces}; do
-        SELECTOR="${SELECTOR} --selector namespace=${namespace}"
-        rm -rf ${OUTPUT_DIR}/cluster/namespaces/${namespace}.yaml
-        rm -rf ${OUTPUT_DIR}/cluster/resources/${namespace}
-        rm -rf ${OUTPUT_DIR}/customresourcedefinitions/${namespace}
-        rm -rf ${OUTPUT_DIR}/namespaces/${namespace}
-      done
-      >&2 echo helmfile with selector ${SELECTOR}
-    fi
+  if echo "$changedNamespaces" | grep -q ^jx$; then
+    >&2 echo jx namespace changed, no selectors added
+  else
+    for namespace in ${changedNamespaces}; do
+      SELECTOR="${SELECTOR} --selector namespace=${namespace}"
+      rm -rf ${OUTPUT_DIR}/cluster/namespaces/${namespace}.yaml
+      rm -rf ${OUTPUT_DIR}/cluster/resources/${namespace}
+      rm -rf ${OUTPUT_DIR}/customresourcedefinitions/${namespace}
+      rm -rf ${OUTPUT_DIR}/namespaces/${namespace}
+    done
+    >&2 echo helmfile with selector ${SELECTOR}
+  fi
 fi
 
 if [ -z "${SELECTOR}" ]; then
