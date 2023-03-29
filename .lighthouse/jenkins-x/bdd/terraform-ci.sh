74577333cbded08d2fef2633ff349e78185feb75
Mårten Svantesson <Marten.Svantesson@ticket.se>
2023-03-29 16:07:35 +0200

    debug: pod_results is not created

--- .lighthouse/jenkins-x/bdd/terraform-ci.sh
+++ .lighthouse/jenkins-x/bdd/terraform-ci.sh
@@ -235,7 +235,7 @@ then
         jx test create -f /workspace/source/.lighthouse/jenkins-x/bdd/$TERRAFORM_FILE --no-watch-job
         tf_resource=tf-${REPO_NAME}-pr${PULL_NUMBER}-${JOB_NAME}-${BUILD_NUMBER}
         aborttime=$(( $(date +%s) + 3600 ))
-        until kubectl get terraforms.tf.isaaguilar.com $tf_resource
+        until kubectl get terraforms.tf.isaaguilar.com $tf_resource -ojsonpath='{.status.phase}'
         do
             if [[ $(date +%s) > $aborttime ]]
             then
