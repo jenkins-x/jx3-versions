819871a839d18b1e776a1a93f3fcad90e27f3771
Skisocks <jaa.skeoch@gmail.com>
2023-12-21 16:56:09 +0000

    feat: compare changed helmfiles with default branch

--- src/get-selectors-and-clean.sh
+++ src/get-selectors-and-clean.sh
@@ -3,26 +3,25 @@
 OUTPUT_DIR=$1
 
 SELECTOR=""
-if ! git log -m -1 --name-only --pretty=format: | grep -qv "^helmfiles/"; then
-  changedNamespaces=$(git log -m -1 --name-only --pretty=format: | grep "^helmfiles/" | cut -d "/" -f 2 | sort -u)
-  if echo "$changedNamespaces" | grep -q ^jx$; then
-    >&2 echo jx namespace changed, no selectors added
-  else
-    for namespace in ${changedNamespaces}; do
-      SELECTOR="${SELECTOR} --selector namespace=${namespace}"
-      rm -rf ${OUTPUT_DIR}/cluster/namespaces/${namespace}.yaml
-      rm -rf ${OUTPUT_DIR}/cluster/resources/${namespace}
-      rm -rf ${OUTPUT_DIR}/customresourcedefinitions/${namespace}
-      rm -rf ${OUTPUT_DIR}/namespaces/${namespace}
-    done
-    >&2 echo helmfile with selector ${SELECTOR}
-  fi
+defaultBranch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+
+if git log -m HEAD ^"$defaultBranch" --name-only --pretty=format: | grep -q "^helmfiles/"; then
+  changedNamespaces=$(git log -m HEAD ^"$defaultBranch" --name-only --pretty=format: | grep "^helmfiles/" | cut -d "/" -f 2 | sort -u)
+  for namespace in ${changedNamespaces}; do
+    SELECTOR="${SELECTOR} --selector namespace=${namespace}"
+    rm -rf ${OUTPUT_DIR}/cluster/namespaces/${namespace}.yaml
+    rm -rf ${OUTPUT_DIR}/cluster/resources/${namespace}
+    rm -rf ${OUTPUT_DIR}/customresourcedefinitions/${namespace}
+    rm -rf ${OUTPUT_DIR}/namespaces/${namespace}
+  done
+  >&2 echo helmfile with selector ${SELECTOR}
 fi
 
 if [ -z "${SELECTOR}" ]; then
   >&2 echo helmfile without selector
   rm -rf ${OUTPUT_DIR}/*/*/
-  jx gitops repository create >&2
 fi
+
+jx gitops repository create >&2
 
 echo ${SELECTOR}
