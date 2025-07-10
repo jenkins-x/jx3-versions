8cf385e672b65d4a77e9c48484304536ba4f2525
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-07-10 19:10:38 +0200

    fix: website update

--- .lighthouse/jenkins-x/release/promote-jx-website.sh
+++ .lighthouse/jenkins-x/release/promote-jx-website.sh
@@ -7,6 +7,9 @@ set -o pipefail
 git clone https://github.com/jenkins-x/jx-docs.git
 
 cd jx-docs
+git config --add user.name ${GIT_AUTHOR_NAME:-jenkins-x-bot}
+git config --add user.email ${GIT_AUTHOR_EMAIL:-jenkins-x@googlegroups.com}
+
 sed -i "s/release = \".*\"/release = \"${JX_VERSION}\"/" config.toml
 if git commit -a -m "chore: upgrade jx version"
 then
